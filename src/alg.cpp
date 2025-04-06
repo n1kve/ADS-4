4fdea7c926b93464921fee0df674ae1976618890
n1kve <nikita06vet@gmail.com>
2025-04-06 15:47:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
-                ++cnt;
+                cnt++;
             }
         }
     }
@@ -13,27 +13,31 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
     int cnt = 0;
-    int l = 0;
-    int r = len - 1;
-    while (l < r) {
-        int sum = arr[l] + arr[r];
-        if (sum == value) {
-            ++cnt;
-            ++l;
-            --r;
-        }
-        else if (sum < value) {
-            ++l;
-        }
-        else {
-            --r;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                cnt++;
+            }
         }
     }
     return cnt;
 }
 
+void bubbleSort(int *arr, int len) {
+    for (int i = 0; i < len - 1; ++i) {
+        for (int j = 0; j < len - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                int t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
+    }
+}
+
 int countPairs3(int *arr, int len, int value) {
     int cnt = 0;
+    bubbleSort(arr, len);
     for (int i = 0; i < len; ++i) {
         int x = value - arr[i];
         int lg = i + 1;
@@ -42,6 +46,11 @@ int countPairs3(int *arr, int len, int value) {
             int sr = (lg + hg) / 2;
             if (arr[sr] == x) {
                 ++cnt;
+                int temp = sr;
+                while (temp - 1 >= lg && arr[temp - 1] == x) {
+                    ++cnt;
+                    --temp;
+                }
                 break;
             }
             else if (arr[sr] < x) {
