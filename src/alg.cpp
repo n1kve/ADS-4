26753e1d15ca2d48d030a85a912544ea36ee6468
n1kve <nikita06vet@gmail.com>
2025-04-06 14:35:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,68 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int cnt = 0;
+    for (int i = 0; i < len; ++i)
+    {
+        for (int j = i + 1; j < len; ++j)
+        {
+            if (arr[i] + arr[j] == value)
+            {
+                ++cnt';
+            }
+        }
+    }
+    return cnt;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int cnt = 0;
+    int l = 0;
+    int r = len - 1;
+    while (l < r)
+    {
+        int sum = arr[l] + arr[r];
+        if (sum == value)
+        {
+            ++cnt;
+            ++l;
+        }
+        else if (sum < value)
+        {
+            ++l;
+        }
+        else
+        {
+            --r;
+        }
+    }
+    
+    return cnt;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int cnt = 0;
+    for (int i = 0; i < len; ++i)
+    {
+        int x = value - arr[i];
+        int lg = i + 1;
+        int hg = len - 1;
+        while (lg <= hg)
+        {
+            int sr = (lg + hg) / 2;
+            if (arr[sr] == x)
+            {
+                ++cnt;
+                break;
+            }
+            else if (arr[sr] < x)
+            {
+                lg = sr + 1;
+            }
+            else
+            {
+                hg = sr - 1;
+            }
+        }
+    }
+    return cnt;
 }
