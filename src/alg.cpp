789a5c6927ef7cb77f9d7fce15d8f77bedab5ba4
n1kve <nikita06vet@gmail.com>
2025-04-06 14:39:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,10 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len; ++i)
-    {
-        for (int j = i + 1; j < len; ++j)
-        {
-            if (arr[i] + arr[j] == value)
-            {
-                ++cnt';
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++cnt;
             }
         }
     }
@@ -18,20 +15,16 @@ int countPairs2(int *arr, int len, int value) {
     int cnt = 0;
     int l = 0;
     int r = len - 1;
-    while (l < r)
-    {
+    while (l < r) {
         int sum = arr[l] + arr[r];
-        if (sum == value)
-        {
+        if (sum == value) {
             ++cnt;
             ++l;
         }
-        else if (sum < value)
-        {
+        else if (sum < value) {
             ++l;
         }
-        else
-        {
+        else {
             --r;
         }
     }
@@ -41,25 +34,20 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len; ++i)
-    {
+    for (int i = 0; i < len; ++i) {
         int x = value - arr[i];
         int lg = i + 1;
         int hg = len - 1;
-        while (lg <= hg)
-        {
+        while (lg <= hg) {
             int sr = (lg + hg) / 2;
-            if (arr[sr] == x)
-            {
+            if (arr[sr] == x) {
                 ++cnt;
                 break;
             }
-            else if (arr[sr] < x)
-            {
+            else if (arr[sr] < x) {
                 lg = sr + 1;
             }
-            else
-            {
+            else {
                 hg = sr - 1;
             }
         }
